cb220c126e6d66f2e82c34c058849ad5781c7dd9
Ryo Okada <atribecalledquest448@gmail.com>
2017-09-12 10:53:21 +0900

    add comment

--- convert_dataset_4_tensorflow.py
+++ convert_dataset_4_tensorflow.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 import collections as cl
-
+import sys
 
 def checkArg():
 
@@ -42,7 +42,8 @@ def generateDataset(type):
         stock_snm_list = df["stock_snm_x"]
         rate_fluctuation_list = df["fluctuation_sell_rate"]
         stock_snm_list_uq = list(set(stock_snm_list))
-        rate_fluctuation_list_uq = list(set(rate_fluctuation_list))
+        # rate_fluctuation_list_uq = list(set(rate_fluctuation_list))
+        rate_fluctuation_list_uq = ["up", "down", "stay"]
         date_list = df["date"]
         date_list_uq = list(set(date_list))
         df2 = convert2number(df, 'mapping_result_stock_snm.txt', stock_snm_list_uq)
@@ -67,6 +68,7 @@ def generateDataset(type):
 
     elif type == "azure":
 
+        df4 = df
         up_sign = "up"
         down_sign = "down"
         stay_sign = "stay"
@@ -100,8 +102,8 @@ def generateDataset(type):
     print ("undersampling ...")
 
     # stayのデータをアンダーサンプリングする
-    df_buy_stay_sampled = df_buy_stay.sample(n=220, replace=True)
-    df_sell_stay_sampled = df_sell_stay.sample(n=220, replace=True)
+    df_buy_stay_sampled = df_buy_stay.sample(n=500, replace=True)
+    df_sell_stay_sampled = df_sell_stay.sample(n=500, replace=True)
 
     ### merge datasets
     df_buy = pd.concat([df_buy_up, df_buy_down, df_buy_stay_sampled])
--- supply_demand_forecast_classifier.py
+++ supply_demand_forecast_classifier.py
@@ -9,7 +9,7 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import confusion_matrix
 # from sklearn.metrics import recall_score, f1_score
 from sklearn.metrics import classification_report
-import seaborn as sns
+# import seaborn as sns
 
 
 # reset computational graph
@@ -22,7 +22,7 @@ if not os.path.exists(read_file):
     print ('can not find csv file...')
     exit()
 
-# read supply demand forecast data into memory
+### データをメモリに読み込む / read supply demand forecast data into memory
 supply_demand_data = []
 with open(read_file, newline='') as csvfile:
     csv_reader = csv.reader(csvfile)
@@ -32,10 +32,10 @@ with open(read_file, newline='') as csvfile:
 
 supply_demand_data = [[float(x) for x in row] for row in supply_demand_data]
 
+
+### 目的変数を抽出 / Extract y-target
 df_dataset = pd.read_csv(read_file)
 y_vals = np.array(df_dataset['fluctuation_buy_rate'])
-
-# Extract y-target
 # y_vals = np.array([x[39] for x in supply_demand_data])
 m = len(y_vals)
 ydata = np.zeros((m, 3))
@@ -43,9 +43,8 @@ for i in range(m):
     ic = int(y_vals[i])
     ydata[i, ic] = 1.0
 
-print(ydata)
 
-# Filter for features of interest
+### 説明変数を抽出 / Filter for features of interest
 cols_of_interest = ['stock_snm_x','contract_count_10','contract_count_11','contract_count_14','contract_count_15','contract_count_20','contract_count_21','contract_count_24','contract_count_25','contract_count_30','contract_count_31','contract_face_10','contract_face_11','contract_face_14','contract_face_15','contract_face_20','contract_face_21','contract_face_24','contract_face_25','contract_face_30','contract_face_31','zanzon','bid','simple_yield_fluctuation_2to1','simple_yield_fluctuation_3to2','amount_sum','balance']
 x_vals = np.array([[x[ix] for ix, feature in enumerate(supply_demand_header) if feature in cols_of_interest] for x in supply_demand_data])
 
@@ -55,9 +54,10 @@ np.random.seed(seed)
 tf.set_random_seed(seed)
 
 # Declare batch size
-batch_size = 90
+# batch_size = 90
+batch_size = 120
 
-# Split data into train/test = 70%/30%
+### データセットをトレーニングセットとテストセットに分割 / Split data into train/test = 70%/30%
 train_indices = np.random.choice(len(x_vals), round(len(x_vals)*0.7), replace=False)
 test_indices = np.array(list(set(range(len(x_vals))) - set(train_indices)))
 x_vals_train = x_vals[train_indices]
@@ -68,7 +68,7 @@ y_vals_train = ydata[train_indices]
 y_vals_test = ydata[test_indices]
 
 
-# Normalize by column (min-max norm)
+### min-maxスケーリングを使って正則化 / Normalize by column (min-max norm)
 def normalize_cols(m):
     col_max = m.max(axis=0)
     col_min = m.min(axis=0)
@@ -86,12 +86,12 @@ x_data = tf.placeholder(shape=[None, 27], dtype=tf.float32)
 y_target = tf.placeholder(shape=[None, 3], dtype=tf.float32)
 
 
-# Create variable definition
+### 変数の定義を作成 / Create variable definition
 def init_variable(shape):
     return(tf.Variable(tf.random_normal(shape=shape)))
 
 
-# Create a logistic layer definition
+### ロジスティック層の定義を作成 / Create a logistic layer definition
 def logistic(input_layer, multiplication_weight, bias_weight, activation = True):
     linear_layer = tf.add(tf.matmul(input_layer, multiplication_weight), bias_weight)
     # We separate the activation at the end because the loss function will
@@ -103,29 +103,40 @@ def logistic(input_layer, multiplication_weight, bias_weight, activation = True)
         # return(tf.nn.softmax(linear_layer))
         
 
-# First logistic layer (7 inputs to 7 hidden nodes)
-A1 = init_variable(shape=[27,60])
-b1 = init_variable(shape=[60])
+### １つ目のロジスティック層（２７個の入力　to 60個の隠れノード）
+A1 = init_variable(shape=[27,100])
+b1 = init_variable(shape=[100])
 logistic_layer1 = logistic(x_data, A1, b1)
 
-# Final output layer (5 hidden nodes to 1 output)
-A3 = init_variable(shape=[60,3])
+# A2 = init_variable(shape=[60, 60])
+# b2 = init_variable(shape=[60])
+# logistic_layer2 = logistic(logistic_layer1, A2, b2)
+
+# A4 = init_variable(shape=[60, 60])
+# b4 = init_variable(shape=[60])
+# logistic_layer3 = logistic(logistic_layer2, A4, b4)
+
+### 最後の出力層（60個の隠れノード to ３つの出力）
+A3 = init_variable(shape=[100,3])
 b3 = init_variable(shape=[3])
 final_output = logistic(logistic_layer1, A3, b3, activation=False)
 
-# Declare loss function (Cross Entropy loss)
+### 損失関数を作成 / Declare loss function (Cross Entropy loss)
 loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=final_output, labels=y_target))
 # cross_entropy = -tf.reduce_sum(y_target*tf.log(final_output))
-# Regularization terms (weight decay)   
+### 正則化 / Regularization terms (weight decay)  
 # L2_sqr = tf.nn.l2_loss(A1) + tf.nn.l2_loss(A3)
 # lambda_2 = 0.01
 # loss = cross_entropy + lambda_2 * L2_sqr
 
-# Declare optimizer
-my_opt = tf.train.AdamOptimizer(learning_rate = 0.002)
+### 最適化関数を作成 / Declare optimizer
+# my_opt = tf.train.AdamOptimizer(learning_rate = 0.001)
 # my_opt = tf.train.GradientDescentOptimizer(learning_rate = 0.001)
+# my_opt = tf.train.AdagradOptimizer(learning_rate = 0.001)
+# my_opt = tf.train.AdadeltaOptimizer(learning_rate = 0.001)
+# my_opt = tf.train.RMSPropOptimizer(learning_rate = 0.001)
+my_opt = tf.train.MomentumOptimizer(0.1,0)
 train_step = my_opt.minimize(loss)
-
 
 
 # Actual Prediction
@@ -144,40 +155,47 @@ summary_op = tf.summary.merge_all()
 summary_writer = tf.summary.FileWriter('log', graph = sess.graph)
 
 
-
 predictions = tf.argmax(prediction, 1)
 actuals = tf.argmax(y_target, 1)
 tf_recall = tf.metrics.recall(actuals, predictions)
-confusion_matrix = tf.confusion_matrix(actuals, predictions)
+confmat = tf.confusion_matrix(actuals, predictions)
 # cl_report = classification_report(actuals, predictions)
 
 
 
-# Initialize variables
+### 変数を初期化 / Initialize variables
 init = tf.global_variables_initializer()
 sess.run(init)
 
-# Training loop
+### Training loop
+### 損失ベクトルと正解ベクトルを初期化
 loss_vec = []
 train_acc = []
 test_acc = []
-for i in range(1500):
+for i in range(3000):
+    ### バッチを選択するためのインデックスをランダムに選択
     rand_index = np.random.choice(len(x_vals_train), size=batch_size)
     # rand_index = np.random.choice(batch_size, batch_size)
+    ### ランダムな値でバッチを取得
     rand_x = x_vals_train[rand_index]
     # rand_y = np.transpose([y_vals_train[rand_index]])
     rand_y = y_vals_train[rand_index]
+
+    ### トレーニングステップを実行
     sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
 
+    ### トレーニングセットの損失値を取得
     temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
     loss_vec.append(temp_loss)
     
+    ### トレーニングセットの正解率を取得
     train_result = sess.run([summary_op, accuracy], feed_dict={x_data: x_vals_train, y_target: y_vals_train})
     summary_str = train_result[0]
     temp_acc_train = train_result[1]
     train_acc.append(temp_acc_train)
     summary_writer.add_summary(summary_str, i)
 
+    ### テストセットの正解率を取得
     temp_acc_test = sess.run(accuracy, feed_dict={x_data: x_vals_test, y_target: y_vals_test})
     test_acc.append(temp_acc_test)
 
@@ -195,6 +213,7 @@ for i in range(1500):
 '''与えられたネットワークの正解率などを出力する。
 '''
 
+'''
 ones_like_actuals = tf.ones_like(actuals)
 zeros_like_actuals = tf.zeros_like(actuals)
 ones_like_predictions = tf.ones_like(predictions)
@@ -263,13 +282,12 @@ print('Precision = ', precision)
 print('Recall = ', recall)
 print('F1 Score = ', f1_score)
 print('Accuracy = ', accuracy)
+'''
 
+### 混合行列 / confusion matrix の表示
 print('-----')
-# con_matrix = sess.run(,
-#     feed_dict={x_data: x_vals_test, y_target: y_vals_test}
-# )
 print(sess.run(
-    confusion_matrix,
+    confmat,
     feed_dict={x_data: x_vals_test, y_target: y_vals_test})
     )
 # print(sess.run(
@@ -277,6 +295,7 @@ print(sess.run(
 #     feed_dict={x_data: x_vals_test, y_target: y_vals_test})
 #     )
 
+### classification report出力
 ac = sess.run(
     actuals,
     feed_dict={x_data: x_vals_test, y_target: y_vals_test})
@@ -289,25 +308,35 @@ print(classification_report(ac, pr))
 
 
 # seaborn.heatmap を使ってプロットする
-# index = list("012")
-# columns = list("012")
-# df = pd.DataFrame(confusion_matrix, index=index, columns=columns)
-
-# fig = plt.figure(figsize = (3,3))
-# sns.heatmap(df, annot=True, square=True, fmt='.0f', cmap="Blues")
-# plt.title('hand_written digit classification')
+# fig, ax = plt.subplots(figsize = (2.5, 2.5))
+# ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+# for i in range(confmat.shape[0]):
+#     for j in range(confmat.shape[1]):
+#         ax.text(x=j, y=i, s=confmat[i,j], va='center', ha='center')
 # plt.xlabel('ground_truth')
 # plt.ylabel('prediction')
-# fig.savefig("conf_mat.png")
+# plt.show()
 
-# Plot loss over time
+index = list("012")
+columns = list("012")
+df = pd.DataFrame(confmat, index=index, columns=columns)
+
+fig = plt.figure(figsize = (3,3))
+sns.heatmap(df, annot=True, square=True, fmt='.0f', cmap="Blues")
+plt.title('hand_written digit classification')
+plt.xlabel('ground_truth')
+plt.ylabel('prediction')
+fig.savefig("conf_mat.png")
+
+
+### 損失値をプロット / Plot loss over time
 plt.plot(loss_vec, 'k-')
 plt.title('Cross Entropy Loss per Generation')
 plt.xlabel('Generation')
 plt.ylabel('Cross Entropy Loss')
 plt.show()
 
-# Plot train and test accuracy
+### トレーニングセットとテストセットの正解率をプロット / Plot train and test accuracy
 plt.plot(train_acc, 'k-', label='Train Set Accuracy')
 plt.plot(test_acc, 'r--', label='Test Set Accuracy')
 plt.title('Train and Test Accuracy')
